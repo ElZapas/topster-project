43c65ea808aa0cad84350ed8921e32f337f19a2c
Anstro <anstrosolano@gmail.com>
2025-04-16 10:36:45 -0500

    Creando rutas principales

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { EditorPage } from "./pages/AlbumEditorPage";
 
-import './App.css'
+export const App = () => (
+  <DndProvider backend={HTML5Backend}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<EditorPage />} />
+      </Routes>
+    </BrowserRouter>
+  </DndProvider>
+);
 
-function App() {
-  return (
-    <>
-    <h1 className='text-4xl text-red-800'>Esta vivo</h1>
-    </>
-  )
-}
-
-export default App
+export default App;
