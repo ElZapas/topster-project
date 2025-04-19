36e3d845e5c212bd1404e8c45f94270a97bc1da5
Anstro <anstrosolano@gmail.com>
2025-04-18 22:44:40 -0500

    Refactorizando el componente TabsContainer

--- src/components/controlPanel/TabsContainer.tsx
+++ src/components/controlPanel/TabsContainer.tsx
@@ -1,21 +1,67 @@
+/**
+ * @file AlbumSlot.tsx
+ * @description Componente que representa un espacio para un álbum en una cuadrícula.
+ * 
+ * Este componente permite arrastrar y soltar álbumes en un espacio específico de la cuadrícula.
+ * También maneja la lógica para mostrar un álbum en el espacio y permite eliminarlo si es necesario.
+ */
+
+/**
+ * @imports
+ * - useState: Hook de React para manejar el estado local del componente.
+ * - gridConfig: Tipo de datos que representa la configuración de la cuadrícula.
+ * - SearchTab: Componente que permite buscar álbumes.
+ * - SettingsTab: Componente que permite ajustar la configuración de la cuadrícula.
+ * - ImportLastFmTab: Componente que permite importar álbumes desde LastFM.
+ * - AboutTab: Componente que muestra información sobre la aplicación.
+ * - Album: Tipo de datos que representa un álbum.
+ */
 import { useState } from "react";
 import { GridConfig } from "../../types/GridConfig";
 import SearchTab from "./search/SearchTab";
-import SettingsTab from "./SettingsTab";
-import ImportLastFmTab from "./ImportLastFmTab";
-import AboutTab from "./AboutTab";
+import SettingsTab from "./settings/SettingsTab";
+import ImportLastFmTab from "./lastfm/ImportLastFmTab";
+import AboutTab from "./about/AboutTab";
 import { Album } from "../../types/album";
 
+/**
+ * @interface TabsContainerProps
+ * @description Propiedades del componente TabsContainer.
+ */
 interface TabsContainerProps {
+  /**
+   * @property gridConfig - Configuración de la cuadrícula.
+   * @property updateGridConfig - Función para actualizar la configuración de la cuadrícula.
+   * @property setAlbums - Función para actualizar el estado de los álbumes.
+   */
+  gridConfig: GridConfig;
   updateGridConfig: React.Dispatch<React.SetStateAction<GridConfig>>;
   setAlbums: React.Dispatch<React.SetStateAction<Album[]>>;
 }
 
+/**
+ * @function TabsContainer
+ * @description Componente que representa un contenedor de pestañas para la aplicación.
+ * 
+ * Este componente permite navegar entre diferentes pestañas: Buscar Álbumes, Ajustes, Importar y Acerca de.
+ * Cada pestaña muestra un componente diferente según la selección del usuario.
+ * 
+ * @param {TabsContainerProps} props - Propiedades del componente.
+ * @returns {JSX.Element} Componente TabsContainer.
+ */
 export const TabsContainer = ({
   updateGridConfig,
-  setAlbums,
+  setAlbums, gridConfig
 }: TabsContainerProps) => {
+  
+  /**
+   * @constant {number} active - Índice de la pestaña activa.
+   */
   const [active, setActive] = useState(0);
+
+  /**
+   * @constant {string[]} tabs - Lista de nombres de las pestañas.
+   */
   const tabs = ["Buscar Álbumes", "Ajustes", "Importar", "Acerca de"];
 
   return (
@@ -39,7 +85,8 @@ export const TabsContainer = ({
       {active === 0 && <SearchTab />}
       {active === 1 && (
         <SettingsTab
-          updateGridConfig={(newConfig) => updateGridConfig(newConfig)}
+        gridConfig={gridConfig}
+        updateGridConfig={(newConfig) => updateGridConfig(newConfig)} // 
         />
       )}
       {active === 2 && <ImportLastFmTab setAlbums={setAlbums} />}
