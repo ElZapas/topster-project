23847fd658c2136b27b5f5a81bd51f5058073d34
Anstro <anstrosolano@gmail.com>
2025-04-18 22:46:30 -0500

    Reintroducir el componente AboutTab con su contenido y estilos originales

--- src/components/controlPanel/AboutTab.tsx
+++
@@ -1,9 +0,0 @@
-export default function AboutTab() {
-  return (
-    <div>
-      <h1>Generador de topsters</h1>
-      <p>Creado por: Fabian Anstro</p>
-      
-    </div>
-  )
-}
---
+++ src/components/controlPanel/about/AboutTab.tsx
@@ -0,0 +1,22 @@
+/**
+  * @file AboutTab.tsx
+  * @description Componente que muestra información sobre el generador de topsters.
+ */
+
+export default function AboutTab() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold text-white mb-4">Generador de topsters</h1>
+      <p className="text-gray-100 mb-2">
+      <span className="font-semibold">Creado por:</span> Fabian Anstro
+      </p>
+      <p className="text-gray-100 mb-4">
+      Este generador de topsters es una herramienta diseñada para ayudarte a crear collages visuales de tus álbumes favoritos. 
+      Puedes personalizar tus listas y compartirlas con tus amigos fácilmente.
+      </p>
+      <p className="text-gray-100">
+      Si tienes alguna sugerencia o encuentras algún problema, no dudes en contactarnos. ¡Gracias por usar nuestra aplicación! :D
+      </p>
+    </div>
+  );
+}
