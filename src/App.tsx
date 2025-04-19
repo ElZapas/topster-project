9d6f05c02c6f0bf56f1f1961fc99571079be7a16
Anstro <anstrosolano@gmail.com>
2025-04-18 22:33:46 -0500

    Modificando archivos principales

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import { DndProvider } from 'react-dnd';
+/**
+ * @file Componente principal de la aplicación.
+ * 
+ * @description Este archivo define el componente `App`, que configura el proveedor de arrastrar y soltar (DndProvider),
+ * el enrutador de la aplicación (BrowserRouter) y las rutas principales. Actualmente, incluye una ruta para la página
+ * del editor de álbumes (`EditorPage`).
+ * 
+ * @returns {JSX.Element} El componente principal de la aplicación.
+ */
+
+/**
+ * @imports
+ * - DndProvider: Proveedor para la funcionalidad de arrastrar y soltar.
+ */
+import { DndProvider } from 'react-dnd'; 
 import { HTML5Backend } from 'react-dnd-html5-backend';
+
+/**
+ * @imports 
+ * - BrowserRouter: Componente de enrutamiento para aplicaciones React.
+ * - Routes: Componente para definir rutas en la aplicación.
+ */
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { EditorPage } from "./pages/AlbumEditorPage";
+
 
 export const App = () => (
   <DndProvider backend={HTML5Backend}>
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,16 @@
+/**
+ * @file Este archivo es el punto de entrada principal de la aplicación React.
+ * @description Renderiza el componente raíz de la aplicación dentro del elemento HTML con el id 'root'.
+ * 
+ * @imports
+ * - StrictMode: Proporciona herramientas para detectar problemas potenciales en la aplicación.
+ * - createRoot: Método de ReactDOM para inicializar la raíz de la aplicación.
+ * - App: Componente principal de la aplicación.
+ * - global.css: Archivo de estilos globales para la aplicación.
+ * 
+ * @usage Este archivo debe mantenerse como el punto de entrada principal y no debe contener lógica adicional.
+ */
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
